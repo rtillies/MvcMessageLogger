b1533c72c08b6af7f4521b33fbb99619c416c0b1
Richard T <richard@turing.edu>
2023-08-08 12:16:23 -0400

    Message #Index and #New routes

---
+++ MvcMessageLogger/Controllers/MessagesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcMessageLogger.DataAccess;
+
+namespace MvcMessageLogger.Controllers
+{
+	public class MessagesController : Controller
+	{
+		private readonly MvcMessageLoggerContext _context;
+
+		public MessagesController(MvcMessageLoggerContext context)
+		{
+			_context = context;
+		}
+
+        [Route("users/{userId:int}/messages")]
+        public IActionResult Index(int userId)
+		{
+            var user = _context.Users
+				.Where(u => u.Id == userId)
+				.Include(u => u.Messages)
+				.First();
+
+            return View(user);
+        }
+
+        [Route("users/{userId:int}/messages/new")]
+        public IActionResult New(int userId)
+		{
+            var user = _context.Users
+                .Where(u => u.Id == userId)
+                .Include(u => u.Messages)
+                .First();
+
+            return View(user);
+        }
+    }
+}
