d4f5553ff03d1d009877e4f37e8b131976d7c120
Richard T <richard@turing.edu>
2023-08-08 10:47:58 -0400

    User #Index

---
+++ MvcMessageLogger/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMessageLogger.DataAccess;
+
+namespace MvcMessageLogger.Controllers
+{
+	public class UsersController : Controller
+	{
+		private readonly MvcMessageLoggerContext _context;
+
+		public UsersController(MvcMessageLoggerContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var users = _context.Users.ToList();
+			return View(users);
+		}
+	}
+}
